035477d56d161cd8d07567a1d3ad548cc3277fa0
eurdream98 <eurdream99@naver.com>
2024-01-02 20:55:55 +0900

    2024-01-02 03_메소드와 api_homework_수정

--- homework/chap03-method-and-api-practice/src/com/ohgiraffers/level02/basic/Application.java
+++ homework/chap03-method-and-api-practice/src/com/ohgiraffers/level02/basic/Application.java
@@ -2,7 +2,8 @@ package com.ohgiraffers.level02.basic;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println(RandomMaker.randomNumber(30,56));
+        System.out.println(RandomMaker.randomNumber(56,30));
+        System.out.println(RandomMaker.randomUpperAlphabet(10));
         System.out.println(RandomMaker.rockPaperScissors());
         System.out.println(RandomMaker.tossCoin());
     }
--- homework/chap03-method-and-api-practice/src/com/ohgiraffers/level02/basic/RandomMaker.java
+++ homework/chap03-method-and-api-practice/src/com/ohgiraffers/level02/basic/RandomMaker.java
@@ -3,28 +3,32 @@ package com.ohgiraffers.level02.basic;
 import java.util.Random;
 
 public class RandomMaker {
-    public static int randomNumber(int min,int max){
-        if(max>=min){
-        int result = (int)(Math.random()*(max-min+1))+min;
+    public static int randomNumber(int min, int max) {
+        int result = max >= min ? (int) (Math.random() * (max - min)) + min : (int) (Math.random() * (min - max)) + max;
         return result;
-        }
-        else{
-            int result = (int)(Math.random()*(min-max+1))+min;
-            return result;
-        }
-        }
-
-    public static String rockPaperScissors(){
-        Random r = new Random();
-        int num = r.nextInt(3);
-        String result = num>0 ? num==1 ? "가위":"바위" : "보";
-    return result;
     }
 
-    public static String tossCoin(){
+    public static String randomUpperAlphabet(int length) {
+        Random r = new Random();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            char alpha = (char) (r.nextInt(26) + 65);
+            sb.append(alpha);
+        }
+        return sb.toString();
+    }
+
+    public static String rockPaperScissors() {
+        Random r = new Random();
+        int num = r.nextInt(3);
+        String result = num > 0 ? num == 1 ? "가위" : "바위" : "보";
+        return result;
+    }
+
+    public static String tossCoin() {
         Random r = new Random();
         int num = r.nextInt(2);
-        String result = num==0?"앞면":"뒷면";
+        String result = num == 0 ? "앞면" : "뒷면";
         return result;
     }
 }
